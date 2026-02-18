867159b6b27cd4c803327706e81fbfed1cab789d
seevani <seevanithapa98@gmail.com>
2026-02-18 14:31:26 +0545

    env file configure

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log ("Hello World")
+const express = require("express");
+const dotenv = required("dotenv");
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+console.log(`Server is runnong on PORT ${PORT}`)
+} );
+
+app.get("/", (req, res) => {
+  res.send("Shivani  .. Backend is running ")
+});
+
+
