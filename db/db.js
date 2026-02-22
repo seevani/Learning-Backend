3cde39037dbd9698b45167f5ff7cc84ddbdfdfdb
seevani <seevanithapa98@gmail.com>
2026-02-22 14:30:06 +0545

    Setup Express server with dotenv and port 5000

---
+++ db/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const DB_URL = process.env.DB_URL;
+
+
+const connectToDatabase = async () => {
+
+    try {
+        await mongoose.connect(DB_URL);
+        console.log("Database is connected")
+    } catch (error) {
+        console.log(`Error while connected db is ${error}`)
+        
+    }
+    console.log(DB_URL);
+
+}
+module.exports = connectToDatabase;
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require("express");
-const dotenv = required("dotenv");
-dotenv.config();
+const connectToDatabase = require("./db/db");
+require("dotenv").config();
 
 const app = express();
+connectToDatabase();
 
-const PORT = process.env.PORT;
-
-app.listen(PORT, () => {
-console.log(`Server is runnong on PORT ${PORT}`)
-} );
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Shivani  .. Backend is running ")
+  res.send("Server running...");
 });
 
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
